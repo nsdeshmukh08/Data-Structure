6e7f464697f55d4fd2709b394b90dafd022ce15a
nsdeshmukh08 <nsdeshmukh08@gmail.com>
2020-05-03 14:17:29 +0530

    Selction sort file.

---
+++ Algorithm/Sorting/SelectionSort.js
@@ -0,0 +1,19 @@
+function SelectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let minIndex;
+    let temp;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[i]) {
+        minIndex = j;
+      }
+    }
+    if (minIndex) {
+      temp = arr[i];
+      arr[i] = arr[minIndex];
+      arr[minIndex] = temp;
+    }
+  }
+  console.log(arr);
+}
+
+SelectionSort([2, 3, 4, 5, 6]);
